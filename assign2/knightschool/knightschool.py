cbb0ae9fc6b5f19149e34ad5783ba6d990bdb07a
Sam Redmond <sredmond@stanford.edu>
2016-05-04 21:48:02 -0700

    better comments for knightschool

--- assign2/knightschool/knightschool.py
+++ assign2/knightschool/knightschool.py
@@ -1,84 +1,109 @@
-import itertools
-import time
-
+#!/usr/bin/env python3 -tt
 """
-It's Winter quarter of your Senior year and you have a few more Ws then you'd like.
+File: knightschool.py
+---------------------
+Assignment 2: Quest for the Holy Grail
+Course: CS 41
+Name: <YOUR NAME>
+SUNet: <SUNet ID>
+
+Replace this with a description of the program.
+
+It's Winter quarter of your Senior year, and you have a few more Ws then you'd like.
 In order for you to 'Camp Stanford' next quarter, you need to take as many classes as
 possible. Lucky for you all classes meet MWF, so you dont have to worry about the
 days of the week, just the times of the day. You need to make a schedule that has
-no overlapping classes, however you dont need to take into account the time of
-getting from one class to another. One last thing, you are fine with classes that
-start at 9, but anything that is later than 6:30 is off the table
+no overlapping classes, however you don't need to take into account the time of
+getting from one class to another. One last thing - you are fine with classes that
+start at 9, but any course that runs later than 6:30 is off the table.
+
+The final key for the next puzzle will be the chronological concatenation of letters
+from your final course list.
+
 The list of possible classes will be listed like this:
 
+    course_name start_time end_time letter
 
-Name start_time end_time piece
+For example,
 
-an example:
-CS41 13.5 15 a
-
-you'll notice that we use decimals for minutes and military time to avoid confusion
-
-You probably can't do this problem with a naive brute force approach that will search all
-possibilities, but there are ways to filter out classes that you would never take. If done
-properly this may leave few enough classes that brute force will work.
-
-Hints:
-
-*   Consider creating a class for a course and reading in the file and creating a list
-    of course objects.
-
-*   Write the brute force method first (going through every possible permutations of classes),
-    then try making it more efficent.
-
-*   Would you ever choose CS110 instead of CS41?
     CS41 13.5 15 a
-    CS110 13.5 16 b
+
+You'll notice that we use decimals for minutes and military time to avoid confusion.
+
+One way to solve this problem involes a naive brute force approach. Look at all possible
+orderings of subsets of classes, and keep the longest one that is valid. However, that approach may
+be too slow. Instead, there are ways to filter out classes that you would never take. When done
+properly, this will leave few enough classes that a brute force approach will work in reasonable
+time.
+
+Notes:
+
+*   Finish the implementation of a class representing a Course. What attributes should
+    instances of this class have?
+
+*   You can define a __str__ or __repr__ method for Courses so that they are more
+    readable when printed to the console.
+
+*   If two classes occur at the same time, choose the class that comes first in the data file.
+
+*   Would you ever choose CourseY instead of CourseX?
+        CourseX 13.5 15 X
+        CourseY 13.5 16 Y
 """
+import itertools
 
-DATA_LOCATION = ""
-
-class Class:
-    def __init__(WHAT_ARE_YOU_GOING_TO_PASS_TO_THE_CONSTRUCTOR):
+class Course:
+    def __init__(self, *what_other_attributes_go_here):
         """
-        What fields should the class have?
+        What attributes should the class have?
         """
         pass
 
-def brute_schedule(classes):
+    def __str__(self):
+        return "Course({})".format(self)
+
+def brute_schedule(courses):
+    """Determine a final course list, subject to time constraints.
+
+    There are a few clever ways to solve this problem, but this function will
+    just implement a brute force approach, and return the final puzzle answer.
     """
-    Unfortunately this problem is not solvable by a greedy algorithm so we have to
-    either do recursion or something a bit more aggressive. The easiest way to find
-    the best schedule is to try to fit as many classes as possible in every
-    conceivable order and then keep the best one.
+    return ''
+
+def fast_schedule(courses):
+    """Filters courses subject to time and overlap constraints.
+
+    For every start time, keep the course with the shortest duration that
+    appears first in the list of courses, and throw out any courses that
+    start earlier than 9 AM or end later than 6:30 PM
+
+    For example, with these three courses:
+
+        CourseA 9 12 A
+        CourseB 9 11.5 B
+        CourseC 9.5 12 C
+
+    CourseB is strictly better than CourseA, so we remove CourseA from
+    consideration, leaving:
+
+        CourseA 9 12 A
+        CourseB 9 11.5 B
+        CourseC 9.5 12 C
+
+    You should return the final puzzle answer.
     """
-    pass
+    # Filter out strictly dominated courses
 
-def fast_schedule(classes):
-    """
-    Takes the classes and keeps only one class per start time with the shortest duration
-
-    For example, with these three classes:
-    ClassA 9 12
-    ClassB 9 11.5
-    ClassC 9.5 12
-
-    This will cut out ClassA becuase you would always prefer ClassB, This leaves:
-    ClassB 9 11.5
-    ClassC 9.5 12
-
-    This *significantly* cuts down the number of permutations
-
-    It also applies the 9am to 6:30pm time constraint
-    """
-    brute_schedule(classes)
+    # With the smallest set of courses, return the final key
+    return brute_schedule(courses)
 
 
 if __name__ == '__main__':
-    with open(DATA_LOCATION, 'r') as f:
-        lines = f.readlines()
+    DATA_FILE = 'courses.txt'
 
-    class_strings = [line.strip() for line in lines]
-    classes = [Class(*class_str.split()) for class_str in class_strings]
+    with open(DATA_FILE, 'r') as f:
+        course_infos = [line.strip() for line in f]
 
-    fast_schedule(classes)
+    courses = [Course(*info.split()) for info in course_infos]
+
+    print(fast_schedule(courses))
