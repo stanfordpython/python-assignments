281bc1975cb24a55a4315da15e541e2d5063000b
Sam Redmond <sredmond@stanford.edu>
2016-05-07 16:44:12 -0700

    Initial commit for Wallscraper

--- assign3/wallscraper.py
+++ assign3/wallscraper.py
@@ -2,4 +2,19 @@
 """
 File: wallscraper.py
 --------------------
+Assignment 3: Wallscraper
+Course: CS 41
+Name: <YOUR NAME>
+SUNet: <SUNet ID>
+
+Replace this with a description of the program.
 """
+import utils
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
