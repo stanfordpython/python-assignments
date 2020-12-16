7bf3c8fb3c982276639e75a7c5a8a16a258dafdd
zsoltlongauer <longauer.zsolt@gmail.com>
2020-12-16 23:14:06 +0200

    Made Knapsack instantiable

--- lab2/knapsack.py
+++ lab2/knapsack.py
@@ -1,36 +1,45 @@
 import random
 
-def getPrivateKey(n):
-    privateKey = []
-    privateKey.append(random.randint(0,35))
-    sumSA = privateKey[0]
-    for _ in range(1, n):
-        nextNum = sumSA + random.randint(0, 15)
-        privateKey.append(nextNum)
-        sumSA += nextNum
-    return privateKey
+class Knapsack:
+    def __init__(self, n):
+        self.privateKey = self.__getPrivateKey__(n)
+        self.publicKey = self.__getPublicKey__(self.privateKey)
+        
 
-def getPublicKey(privateKey, m, n):
-    publicKey = []
-    for i in privateKey:
-        publicKey.append((i * n)%m)
-    return publicKey
+    def __getPrivateKey__(self, n):
+        privateKey = []
+        privateKey.append(random.randint(0,35))
+        sumSA = privateKey[0]
+        for _ in range(1, n):
+            nextNum = sumSA + random.randint(0, 15)
+            privateKey.append(nextNum)
+            sumSA += nextNum
+        return privateKey
 
-def defineM(privateKey):
-    return sum(privateKey) + random.randint(0, 10)
+    def __getPublicKey__(self, privateKey):
+        m = self.__defineM__(privateKey)
+        n = self.__defineN__(m)
 
-def defineN(m):
-    x = 2
-    n = 0
-    while x != 1:
-        n = random.randint(0, m)
-        x = n
-        y = m
+        publicKey = []
+        for i in privateKey:
+            publicKey.append((i * n)%m)
+        return publicKey
 
-        while(y):
-            x,y=y,x%y
-    return n
+    def __defineM__(self, privateKey):
+        return sum(privateKey) + random.randint(0, 10)
 
-# print(superIncreasingArray(25))
+    def __defineN__(self, m):
+        x = 2
+        n = 0
+        while x != 1:
+            n = random.randint(0, m)
+            x = n
+            y = m
 
-print(defineN(2000000))
+            while(y):
+                x,y=y,x%y
+        return n
+
+knap = Knapsack(10)
+print(knap.privateKey)
+print(knap.publicKey)
